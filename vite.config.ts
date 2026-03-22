520b5ff5f83eb2db9a3a0bc6519c7deab02018b1
freall <1667586+freall@users.noreply.github.com>
2026-03-22 18:00:42 +0800

    fix: 兼容自定义域名与子路径部署

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
 
 const isProd = process.env.BUILD_MODE === 'prod'
 export default defineConfig({
-  base: '/math-animation/',
+  base: './',
   plugins: [
     react(),
     sourceIdentifierPlugin({
